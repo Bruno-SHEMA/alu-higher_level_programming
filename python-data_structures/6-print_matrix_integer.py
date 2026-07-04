715d4aa6e00d6013f04ea1c92f5aa10c7d0dcc32
Bruno-SHEMA <b.shema3@alustudent.com>
2026-07-04 04:27:14 -0700

    Remainig tasks

---
+++ python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix[[]]):
+    for row in matrix:
+        for i, val in enumerate(row):
+            if != 0:
+                print(" ", end="")
+        print("{:d}".format(val), end="")
+    print()
+

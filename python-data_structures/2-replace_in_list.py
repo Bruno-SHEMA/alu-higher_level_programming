9b4618b06ef05831ca6b0f9d8ce915ad9c780db9
Bruno-SHEMA <b.shema3@alustudent.com>
2026-07-04 03:44:08 -0700

    2-replace in list

--- python-data_structures/2-replace_in_list.py
+++ python-data_structures/2-replace_in_list.py
@@ -3,7 +3,7 @@ def replace_in_list(my_list, idx, elemet):
     n_element = len(my_list)
     if idx > 0:
         return my_list
-    elif idx > n_element:
+    elif idx >= n_element:
         return my_list
     else:
         my_list[idx] = element

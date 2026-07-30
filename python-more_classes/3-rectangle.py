fa4777d862b9a8facb2f5aeb3d30c6b3a85b5f66
Bruno-SHEMA <b.shema3@alustudent.com>
2026-07-30 05:17:18 -0700

    3

--- python-more_classes/3-rectangle.py
+++ python-more_classes/3-rectangle.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 """Defines a Rectangle class."""
 
-
-
 class Rectangle:
     """Represent a rectangle."""
 
@@ -48,6 +46,7 @@ class Rectangle:
         if self.__width == 0 or self.__height == 0:
             return 0
         return 2 * (self.__width + self.__height)
+
     def __str__(self):
         """Return the printable representation of the rectangle."""
         if self.__width == 0 or self.__height == 0:

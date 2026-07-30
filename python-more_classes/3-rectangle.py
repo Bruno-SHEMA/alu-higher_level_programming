3f08ccd98c271c39f288983586291316cfa7b5ed
Bruno-SHEMA <b.shema3@alustudent.com>
2026-07-30 05:10:57 -0700

    Debbugging 3-p

--- python-more_classes/3-rectangle.py
+++ python-more_classes/3-rectangle.py
@@ -1,9 +1,9 @@
-def __str__(self):
-    """Return the printable representation of the rectangle."""
-    if self.__width == 0 or self.__height == 0:
-        return ""
+    def __str__(self):
+        """Return the printable representation of the rectangle."""
+        if self.__width == 0 or self.__height == 0:
+            return ""
 
-    rect = []
-    for i in range(self.__height):
-        rect.append("#" * self.__width)
-    return "\n".join(rect)
+        rect = []
+        for i in range(self.__height):
+            rect.append("#" * self.__width)
+        return "\n".join(rect)

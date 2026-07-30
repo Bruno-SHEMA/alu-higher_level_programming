70d51f11c9321f683e56e234f99844a0d17b2996
Bruno-SHEMA <b.shema3@alustudent.com>
2026-07-30 05:22:18 -0700

    finishing 4

--- python-more_classes/4-rectangle.py
+++ python-more_classes/4-rectangle.py
@@ -47,6 +47,7 @@ class Rectangle:
         if self.__width == 0 or self.__height == 0:
             return 0
         return 2 * (self.__width + self.__height)
+
     def __str__(self):
         """Return the printable representation of the rectangle."""
         if self.__width == 0 or self.__height == 0:
@@ -56,3 +57,7 @@ class Rectangle:
         for i in range(self.__height):
             rect.append("#" * self.__width)
         return "\n".join(rect)
+
+   def __repr__(self):
+       """Return a string representation of the rectangle."""
+       return "Rectangle({}, {})".format(self.__width, self.__height)

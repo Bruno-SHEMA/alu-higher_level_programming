962948e3e76486e75ed8c8a99458ea55becdd47e
Bruno-SHEMA <b.shema3@alustudent.com>
2026-07-30 05:34:10 -0700

    4

--- python-more_classes/4-rectangle.py
+++ python-more_classes/4-rectangle.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Defines a Rectangle class."""
 
+
 class Rectangle:
     """Represent a rectangle."""
 
@@ -57,6 +58,6 @@ class Rectangle:
             rect.append("#" * self.__width)
         return "\n".join(rect)
 
-   def __repr__(self):
-       """Return a string representation of the rectangle."""
-       return "Rectangle({}, {})".format(self.__width, self.__height)
+    def __repr__(self):
+        """Return a string representation of the rectangle."""
+        return "Rectangle({}, {})".format(self.__width, self.__height)

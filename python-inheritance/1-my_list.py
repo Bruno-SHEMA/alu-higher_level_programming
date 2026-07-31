9900b6cb05eb692678a2614ff40827e0c50f0436
Bruno-SHEMA <b.shema3@alustudent.com>
2026-07-31 01:06:33 -0700

    Finishing inheritance

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -6,5 +6,5 @@ class MyList(list):
     """A list subclass with a print_sorted method."""
 
     def print_sorted(self):
-        """Print the list in ascending sorted order."""
+        """Print the list in ascending_sorted order."""
         print(sorted(self))
